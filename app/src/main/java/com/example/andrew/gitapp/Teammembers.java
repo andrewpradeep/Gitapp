271b7aff6fb7edabe237e5bc6b006df7ad563549
andrewpradeep <jawaharpradeep001@gmail.com>
2017-03-01 13:43:42 +0530

    intent put extra

--- app/src/main/java/com/example/andrew/gitapp/Teammembers.java
+++ app/src/main/java/com/example/andrew/gitapp/Teammembers.java
@@ -2,13 +2,19 @@ package com.example.andrew.gitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Teammembers extends AppCompatActivity {
-    public String s1,s2,s3;
+    public String overs,noplay,nowide;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teammembers);
+        overs=getIntent().getExtras().getString("overs");
+        noplay=getIntent().getExtras().getString("noplay");
+        nowide=getIntent().getExtras().getString("nowide");
+        Toast.makeText(Teammembers.this," "+overs+" "+noplay+" "+nowide+" ",Toast.LENGTH_LONG).show();
     }
 }
